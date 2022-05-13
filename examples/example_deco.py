e65846b3dca4236fa9f7af524ab7315a4d546a10
Ksenia <ksenia.briling@epfl.ch>
2022-05-13 17:36:29 +0200

    Save density to a .molden file

--- examples/example_deco.py
+++ examples/example_deco.py
@@ -1,6 +1,7 @@
 import os
 from qstack import compound, fields
 from qstack.fields.decomposition import get_coeff
+from qstack.fields.density2file import coeffs_to_cube, coeffs_to_molden
 
 path = os.path.dirname(os.path.realpath(__file__))
 mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'def2svp', charge=0, spin=0)
@@ -14,4 +15,11 @@ c = get_coeff(dm, eri2c, eri3c)
 print("Expansion Coefficients:", c)
 
 N = fields.decomposition.number_of_electrons_deco(auxmol, c)
+
 print("Number of electrons after decomposition: ", N)
+
+coeffs_to_cube(auxmol, c, 'H2O.cube')
+print('density saved to H2O.cube')
+
+coeffs_to_molden(auxmol, c, 'H2O.molden')
+print('density saved to H2O.molden')
--- qstack/fields/density2file.py
+++ qstack/fields/density2file.py
@@ -1,6 +1,8 @@
-import numpy as np
+import numpy
 from pyscf.dft.numint import eval_ao
 from pyscf.tools.cubegen import Cube
+import pyscf.tools.molden
+from qstack.fields.decomposition import number_of_electrons_deco
 
 def coeffs_to_cube(mol, coeffs, cubename, nx = 80, ny = 80, nz = 80, resolution = 0.1, margin = 3.0):
 
@@ -10,10 +12,21 @@ def coeffs_to_cube(mol, coeffs, cubename, nx = 80, ny = 80, nz = 80, resolution 
     # Compute density on the .cube grid
     coords = grid.get_coords()
     ngrids = grid.get_ngrids()
-    
+
     ao = eval_ao(mol, coords)
-    orb_on_grid = np.dot(ao, coeffs)
+    orb_on_grid = numpy.dot(ao, coeffs)
     orb_on_grid = orb_on_grid.reshape(grid.nx,grid.ny,grid.nz)
 
     # Write out orbital to the .cube file
     grid.write(orb_on_grid, cubename, comment='Electron Density')
+
+
+def coeffs_to_molden(mol, coeffs, moldenname):
+    with open(moldenname, 'w') as f:
+        pyscf.tools.molden.header(mol, f, True)
+        try:
+            N = number_of_electrons_deco(mol, coeffs)
+        except:
+            N = 0.0
+        pyscf.tools.molden.orbital_coeff(mol, f, numpy.array([coeffs]).T, ene=[0.0], occ=[N], ignore_h=True)
+
