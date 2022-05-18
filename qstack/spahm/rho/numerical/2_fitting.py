ccccaf633b4ce0da096b7b34d5b427fae2941e51
Ksenia <ksenia.briling@epfl.ch>
2022-05-18 19:37:23 +0200

    [spahm-rho] use qstack

--- qstack/spahm/rho/numerical/2_fitting.py
+++ qstack/spahm/rho/numerical/2_fitting.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 import sys
 import numpy as np
-from pyscf import dft,df,tools
-import pyscf_ext
+from qstack import compound, fields
 
 basis  = 'ccpvdz'
 basis2 = 'ccpvdz jkfit'
 
-mol = pyscf_ext.readmol(sys.argv[1], basis)
-
-mf = dft.RKS(mol)
-mf.xc = 'pbe'
-
-mf.run()
-
-auxmol = df.make_auxmol(mol, basis2)
-e2c = auxmol.intor('int2c2e_sph')
-e3c = pyscf_ext.eri_pqi(mol, auxmol)
-w   = np.einsum('pq,qpi->i', mf.make_rdm1(), e3c)
-c   = np.linalg.solve(e2c, w)
-
+mol       = compound.xyz_to_mol(sys.argv[1], basis)
+dm        = fields.dm.get_converged_dm(mol, xc="pbe")
+auxmol, c = fields.decomposition.decompose(mol, dm, basis2)
 np.save(sys.argv[1]+'.c.npy', c)
--- qstack/spahm/rho/numerical/README.bash
+++ qstack/spahm/rho/numerical/README.bash
@@ -4,7 +4,7 @@ mkdir 36/
 mv 1.xyz.* 36
 
 # compute all the DF coefficients:
-for i in 36/*.xyz.?? ; do ./fitting.py $i ; done
+for i in 36/*.xyz.?? ; do ./2_fitting.py $i ; done
 
 # compute the overlap of the density
 # attributed to the 1st atom of 1.xyz and the 1st atom of 2.xyz
