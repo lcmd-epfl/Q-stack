f1dd82dcf46b9e60085ceb805006ada3624ea6b3
Ksenia <ksenia.briling@epfl.ch>
2022-05-27 19:42:21 +0200

    [spahm-rho] Change the merging script

--- qstack/spahm/rho/01_bind.py
+++ qstack/spahm/rho/01_bind.py
@@ -1,48 +1,30 @@
-import numpy as np
-import sys
-from os.path import join
-from os import getcwd
-import argparse
+#!/usr/bin/env python3
 
+import os
+import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='This script generates a single file per atomic species binding all the feature-vector representations obtained using the single-structures representations generator script (1_DMbRep.py)')
-
-parser.add_argument('--X-dir', required = True, type = str, dest='XDir', help = 'The path to the directory containing all the single-structure X representations.')
-parser.add_argument('--species', required = False, type = str, nargs='+', dest='Species', help = 'The species contained in the DatBase.')
-
+parser.add_argument('--X-dir',   required = True,                     type = str,            dest='XDir', help = 'The path to the directory containing all the single-structure X representations.')
+parser.add_argument('--species', default = ["C", "H", "N", "O", "S"], type = str, nargs='+', dest='Species', help = 'The species contained in the DatBase.')
 args = parser.parse_args()
 
-# def main() :
-cwd = getcwd()
+species = list(sorted(args.Species))
+X_files = sorted(os.listdir(args.XDir))
 
-list_ordered = np.loadtxt('list_structures.txt', dtype=str)
+X_atom = {}
+for e in species:
+    X_atom[e] = []
 
-X_dir = join(cwd, args.XDir)
-
-X_files = [join(X_dir, 'X_' + mol.split('.')[0] + '.npy') for mol in list_ordered]
-
-# print(X_files)
-
-if args.Species :
-	species = list(sorted(args.Species))
-else :
-	species = sorted(["C", "H", "N", "O", "S"])
-X_atom = dict()
-for e in species :
-	X_atom[e] = []
-print(X_atom)
 for f in X_files :
-	X = np.load(f, allow_pickle=True)
-	for a in X :
-		X_atom[a[0]].append(a[1])
-	print(f, "binded", sep=' ')
+    X = np.load(args.XDir+'/'+f, allow_pickle=True)
+    for q,x in X :
+        X_atom[q].append(x)
+    print(f)
 
 for e in X_atom.keys() :
-	X_array = np.array(X_atom[e])
-	name_out = 'X_' + e
-	np .save(join(cwd, name_out), X_array)
-	print('# Atoms for ', e, '=', X_array.shape, sep='\t')
-
-
-# if __name__ == '__main__ ' : main()
-
+    X_array = np.array(X_atom[e])
+    name_out = 'X_' + e + '_' + args.XDir
+    if name_out[-1] == '/': name_out = name_out[:-1]
+    np.save(name_out, X_array)
+    print('# Atoms for ', e, '=', X_array.shape)
