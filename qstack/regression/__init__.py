fd880ef853174decb6d4f91393881bd42a1ff66f
Ksenia <ksenia.briling@epfl.ch>
2022-06-15 16:48:49 +0200

    Minor fixes

--- qstack/regression/__init__.py
+++
@@ -1,1 +0,0 @@
-from qstack.regression import kernel
--- qstack/regression/kernel.py
+++ qstack/regression/kernel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 from qstack.regression.kernel_utils import get_kernel, defaults
+from qstack.tools import correct_num_threads
 
 def kernel(X, sigma=defaults.sigma, akernel=defaults.kernel):
     kernel = get_kernel(akernel)
@@ -15,8 +16,10 @@ def main():
     parser.add_argument('--sigma',  type=float, dest='sigma',     default=defaults.sigma,  help='sigma hyperparameter (default='+str(defaults.sigma)+')')
     parser.add_argument('--kernel', type=str,   dest='kernel',    default=defaults.kernel, help='kernel type (G for Gaussian, L for Laplacian, myL for Laplacian for open-shell systems) (default '+defaults.kernel+')')
     parser.add_argument('--dir',    type=str,   dest='dir',       default='./',            help='directory to save the output in (default=current dir)')
+    parser.add_argument('--ll',     action='store_true', dest='ll', default=False,  help='if correct for the numper of threads')
     args = parser.parse_args()
     print(vars(args))
+    if(args.ll): correct_num_threads()
     X = np.load(args.repr)
     K = kernel(X, args.sigma, args.kernel)
     np.save(args.dir+'/K_'+os.path.basename(args.repr)+'_'+args.kernel, K)
