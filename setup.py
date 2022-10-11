d3df943ae21abc473926d5679e909c233faf4647
Ksenia <ksenia.briling@epfl.ch>
2022-10-11 16:01:41 +0200

    dummy setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='qstack',
+    version='0.0.0',
+    description='Stack of codes for dedicated pre- and post-processing tasks for Quantum Machine Learning',
+    url='https://github.com:lcmd-epfl/Q-stack.git',
+    install_requires=[],
+    packages=['qstack'],
+)
