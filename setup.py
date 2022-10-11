ec5cfd38b46a6d470ada6ddfacb688de14f8925e
Ksenia <ksenia.briling@epfl.ch>
2022-10-11 16:26:49 +0200

    How to include subdirs?

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='qstack',
     version='0.0.0',
     description='Stack of codes for dedicated pre- and post-processing tasks for Quantum Machine Learning',
-    url='https://github.com:lcmd-epfl/Q-stack.git',
+    url='https://github.com/lcmd-epfl/Q-stack',
     install_requires=[],
-    packages=['qstack'],
+    packages=find_packages(),
 )
