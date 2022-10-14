a6d5e31ede14964d315d40119e277c3e6431e156
Ksenia <ksenia.briling@epfl.ch>
2022-10-14 13:49:45 +0200

    Fix the OTPD test

--- tests/test_otpd.py
+++ tests/test_otpd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 from qstack import compound, fields
 
@@ -5,8 +7,10 @@ def test_hf_otpd():
 
     path = os.path.dirname(os.path.realpath(__file__))
     mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'def2svp', charge=0, spin=0)
+    dm = fields.dm.get_converged_dm(mol, xc="pbe")
+    otpd, grid = fields.hf_otpd.hf_otpd(mol, dm, return_all=True)
+    assert(abs(otpd @ grid.weights-20.190382555868)<1e-10)
 
-    dm = fields.dm.get_converged_dm(mol,xc="pbe")
-    otpd = fields.hf_otpd.hf_otpd(mol, dm)
 
-    assert(otpd.shape[0] == 34310)
+if __name__ == '__main__':
+    test_hf_otpd()
