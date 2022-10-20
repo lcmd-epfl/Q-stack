11a90c521e07c35bbee5108f9197159a52859004
YAY-C <yannick.calvinoalonso@epfl.ch>
2022-10-20 18:15:49 +0200

    completed tests for avg and rem kernel

--- tests/test_global.py
+++ tests/test_global.py
@@ -3,23 +3,32 @@ import numpy as np
 from qstack.regression import kernel, kernel_utils
 
 
-## TODO: complete the test for global kernel computations; this is just a copy from test_saphm()
 
 def test_avg_kernel():
 
     path = os.path.dirname(os.path.realpath(__file__))
     X_dir = os.path.join(path, 'data/SPAHM_a_H2O/')
     mols = [np.load(os.path.join(X_dir, f), allow_pickle=True) for f in os.listdir(X_dir) if os.path.isfile(os.path.join(X_dir,f))]
-
     K = kernel.kernel(mols, akernel='L', gkernel='avg', sigma=1.0)
 
-    true_K = np.array(  [[1.       ,  0.58784717, 0.58784717], \
-                        [0.58784717, 1.         , 0.74535599], \
-                        [0.58784717, 0.74535599 , 1.        ]])
-    print(K)
+    true_K = np.array(  [[1.       ,  1.       ,  0.79179528], \
+                        [1.        , 1.        , 0.79179528] , \
+                        [0.79179528, 0.79179528, 1.        ]])
+
 
     assert(K.shape == (3,3))
     assert(np.abs(np.sum(K-true_K)) < 1e-05)
 
-test_avg_kernel()
+def test_rem_kernel():
+    path = os.path.dirname(os.path.realpath(__file__))
+    X_dir = os.path.join(path, 'data/SPAHM_a_H2O/')
+    mols = [np.load(os.path.join(X_dir, f), allow_pickle=True) for f in os.listdir(X_dir) if os.path.isfile(os.path.join(X_dir,f))]
+    K = kernel.kernel(mols, akernel='L', gkernel='rem', sigma=1.0, gdict={'alpha':1.0, 'normalize':1})
+
+    true_K = np.array(  [[1.      ,  1.      ,  0.6528238], \
+                        [1.       , 1.       , 0.6528238] , \
+                        [0.6528238, 0.6528238, 1.       ]])
+
+    assert(K.shape == (3,3))
+    assert(np.abs(np.sum(K-true_K)) < 1e-05)
 
