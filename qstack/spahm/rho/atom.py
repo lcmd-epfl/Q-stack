7c45c3218d5a461b578815e8e2aeb2a4e4834f55
YAY-C <yannick.calvinoalonso@epfl.ch>
2023-02-02 16:46:09 +0100

    added units argument and spin-handling

--- qstack/spahm/rho/atom.py
+++ qstack/spahm/rho/atom.py
@@ -5,8 +5,9 @@ import sys
 import os
 import numpy as np
 from qstack import compound, spahm
-from . import utils, dmb_rep_atom as dmba
-from .utils import defaults
+from qstack.tools import unix_time_decorator
+import utils, dmb_rep_atom as dmba
+from utils import defaults
 
 
 def check_file(mol_file):
@@ -48,6 +49,7 @@ def main():
     parser = argparse.ArgumentParser(description='Script intended for computing Density-Matrix based representations (DMbReps) for efficient quantum machine learning.')
     parser.add_argument('--mol',       dest='mol',       required=True,                        type=str, help="The path to the xyz file with the molecular structure")
     parser.add_argument('--guess',     dest='guess',     default=defaults.guess,               type=str, help=f"The initial guess Hamiltonian to be used (default: {defaults.guess}")
+    parser.add_argument('--units',     dest='units',     default='Angstrom',               type=str, help=f"The units of the given coordinates files (default: Angstrom")
     parser.add_argument('--basis-set', dest='basis',     default=defaults.basis,               type=str, help="Basis set for computing density matrix (default: {defaults.basis})")
     parser.add_argument('--aux-basis', dest='auxbasis',  default=defaults.auxbasis,            type=str, help=f"Auxiliary basis set for density fitting (default: {defaults.auxbasis}")
     parser.add_argument('--model',     dest='model',     default=defaults.model,               type=str, help="The model to use when creating the representation (default: {defaults.model})")
@@ -56,13 +58,13 @@ def main():
     parser.add_argument('--charge',    dest='charge',    default=0,                            type=int, help='total charge of the system (default: 0)')
     parser.add_argument('--spin',      dest='spin',      default=None,                         type=int, help='number of unpaired electrons (default: None) (use 0 to treat a closed-shell system in a UHF manner)')
     parser.add_argument('--xc',        dest='xc',        default=defaults.xc,                  type=str, help='DFT functional for the SAD guess (default=HF)')
-    parser.add_argument('--out',       dest='NameOut',   default=None,                         type=str, help='name of the outpute representations file.')
+    parser.add_argument('--nameout',       dest='NameOut',   default=None,                         type=str, help='name of the outpute representations file.')
     parser.add_argument('--omod',      dest='omod',      default=defaults.omod,     nargs='+', type=str, help='model(s) for open-shell systems (alpha, beta, sum, diff')
     args = parser.parse_args()
     print(vars(args))
 
     elements = sorted(list(set(args.elements)))
-    mol = compound.xyz_to_mol(check_file(args.mol), args.basis, charge=args.charge, spin=args.spin)
+    mol = compound.xyz_to_mol(check_file(args.mol), args.basis, charge=args.charge, spin=args.spin, unit=args.units)
     dm = None if args.dm is None else np.load(args.dm)
 
     representations = get_repr(mol, elements, args.charge, args.spin,
@@ -71,28 +73,31 @@ def main():
 
     # output dir
     cwd = os.getcwd()
-    dir_out = 'X_' + (cwd+'/'+args.mol).split('/')[-2]
-    if not os.path.isdir(os.path.join(cwd, dir_out)):
-        os.mkdir(os.path.join(cwd, dir_out))
 
     # save the output
     mol_name = args.mol.split('/')[-1].split('.')[0]
 
-    for omod, vectors in zip(args.omod, representations):
+    if args.spin != None:
+        for omod, representations in zip(args.omod, representations):
+            if args.NameOut is not None:
+                name_out = args.NameOut
+            else:
+                name_out = 'X_'+mol_name
+            name_out = name_out+'_'+omod
+            path_out = os.path.join(cwd, name_out)
+            np.save(path_out, representations)
+    else:
         if args.NameOut is not None:
             name_out = args.NameOut
         else:
             name_out = 'X_'+mol_name
-        if not args.spin:
-            name_out = name_out+'_'+omod
-        path_out = os.path.join(cwd, dir_out, name_out)
-        np.save(path_out, vectors)
-        if args.spin is None:
-            break
+        path_out = os.path.join(cwd, name_out)
+        np.save(path_out, representations)
+
 
     print(f"Generated density-based representation for {mol_name} with")
     print("Type.\tlength")
-    for q, v in vectors:
+    for q, v in representations:
         print(q+'\t'+str(len(v)))
     print(f"stored at : {path_out}\n")
 
