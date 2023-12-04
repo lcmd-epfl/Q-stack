ffcf669bf6fa1d039bae356bf35e1a175b897ad9
Ksenia <ksenia.briling@epfl.ch>
2023-12-04 18:18:02 +0100

    Fixup 6020610: shorten the test

--- tests/test_slatm.py
+++ tests/test_slatm.py
@@ -7,7 +7,7 @@ from qstack.qml.slatm import get_slatm_for_dataset
 def test_slatm_global():
     path = os.path.dirname(os.path.realpath(__file__))
     v0 = np.load(f'{path}/data/slatm/slatm_global.npy')
-    xyzs = [f for f in sorted(glob.glob(f"{path}/data/slatm/*.xyz"))]
+    xyzs = sorted(glob.glob(f"{path}/data/slatm/*.xyz"))
     v = get_slatm_for_dataset(xyzs, progress=False, global_repr=True)
     assert(np.linalg.norm(v-v0)<1e-10)
 
@@ -15,7 +15,7 @@ def test_slatm_global():
 def test_slatm_local():
     path = os.path.dirname(os.path.realpath(__file__))
     v0 = np.load(f'{path}/data/slatm/slatm_local.npy')
-    xyzs = [f for f in sorted(glob.glob(f"{path}/data/slatm/*.xyz"))]
+    xyzs = sorted(glob.glob(f"{path}/data/slatm/*.xyz"))
     v = get_slatm_for_dataset(xyzs, progress=False)
     assert(np.linalg.norm(v-v0)<1e-10)
 
