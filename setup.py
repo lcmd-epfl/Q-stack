66d0e22639c34bf773fc18b4ee5ca92d14c67d18
Ksenia Briling <30023616+briling@users.noreply.github.com>
2024-02-08 23:20:06 +0100

    Try to get version automatically

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup, find_packages, Extension
 
+def get_git_version_hash():
+    """Get tag/hash of the latest commit.
+    Thanks to https://gist.github.com/nloadholtes/07a1716a89b53119021592a1d2b56db8"""
+    try:
+        p = subprocess.Popen(["git", "describe", "--tags", "--dirty", "--always"], stdout=subprocess.PIPE)
+    except EnvironmentError:
+        print("Cannot run git to get a version number")
+        return "0.0.0-unknown"
+    version = p.communicate()[0]
+    return version.strip()
+
 setup(
     name='qstack',
-    version='0.0.0',
+    version=get_git_version_hash(),
     description='Stack of codes for dedicated pre- and post-processing tasks for Quantum Machine Learning',
     url='https://github.com/lcmd-epfl/Q-stack',
     install_requires=[],
