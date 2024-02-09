ac79a87b97bea04c4087a2d27f7682267aa44887
Ksenia <ksenia.briling@epfl.ch>
2024-02-09 12:23:16 +0100

    Build C extensions with OpenMP only if available

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages, Extension
 import subprocess
+import os, tempfile, shutil
 
 VERSION="0.0.1"
+
 
 def get_git_version_hash():
     """Get tag/hash of the latest commit.
@@ -15,19 +17,38 @@ def get_git_version_hash():
     return VERSION+'+'+version.strip().decode()
 
 
-setup(
-    name='qstack',
-    version=get_git_version_hash(),
-    description='Stack of codes for dedicated pre- and post-processing tasks for Quantum Machine Learning',
-    url='https://github.com/lcmd-epfl/Q-stack',
-    install_requires=[],
-    packages=setuptools.find_packages(exclude=['tests', 'examples']),
-    ext_modules=[Extension('qstack.regression.lib.manh',
-                           ['qstack/regression/lib/manh.c'],
-                           extra_compile_args=['-fopenmp'],
-                           extra_link_args=['-lgomp'])
-                ],
-    include_package_data=True,
-    package_data={'': ['regression/lib/manh.c']},
-)
+def check_for_openmp():
+    """Check if there is OpenMP available
+    Thanks to https://stackoverflow.com/questions/16549893/programatically-testing-for-openmp-support-from-a-python-setup-script"""
+    omp_test = 'void main() { }'
+    tmpdir = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(tmpdir)
+    filename = r'test.c'
+    with open(filename, 'w') as file:
+        file.write(omp_test)
+    with open(os.devnull, 'w') as fnull:
+        result = subprocess.call(['cc', '-fopenmp', '-lgomp', filename], stdout=fnull, stderr=fnull)
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir)
+    return not result
 
+
+if __name__ == '__main__':
+    openmp_enabled = check_for_openmp()
+
+    setup(
+        name='qstack',
+        version=get_git_version_hash(),
+        description='Stack of codes for dedicated pre- and post-processing tasks for Quantum Machine Learning',
+        url='https://github.com/lcmd-epfl/Q-stack',
+        install_requires=[],
+        packages=setuptools.find_packages(exclude=['tests', 'examples']),
+        ext_modules=[Extension('qstack.regression.lib.manh',
+                               ['qstack/regression/lib/manh.c'],
+                               extra_compile_args=['-fopenmp'] if openmp_enabled else [],
+                               extra_link_args=['-lgomp'] if openmp_enabled else [])
+                    ],
+        include_package_data=True,
+        package_data={'': ['regression/lib/manh.c']},
+    )
