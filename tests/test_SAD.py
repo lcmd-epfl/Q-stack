0f4ba3988a96ea681bdd7134f3a27146c9a94916
calvinoY <yannick.calvinoalonso@epfl.ch>
2024-04-23 17:47:38 +0200

    refined failed test (water example)

--- tests/test_SAD.py
+++ tests/test_SAD.py
@@ -6,10 +6,10 @@ from qstack import compound
 from qstack.spahm.rho import atom
 
 
-def test_water():
+def test_water_open_shell():
     print("Running water-test")
     path = os.path.dirname(os.path.realpath(__file__))
-    mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'sto3g', charge=0, spin=0)
+    mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'sto3g', charge=1, spin=1) ## test breaks when effective open-shell caluclation is needed
 
     Xsad = atom.get_repr(mol, ["H", "O"], 0, 0, dm=None,
                       xc = 'hf', guess='sad', model='lowdin-long-x', auxbasis='ccpvdzjkfit')
@@ -18,18 +18,31 @@ def test_water():
     return 0
 
 
-def test_other():
-    print("Running weird-test")
+def test_water_close_shell():
+    print("Running water-test")
     path = os.path.dirname(os.path.realpath(__file__))
-    mol = compound.xyz_to_mol(path+'/data/645688c7c139c6414b5c0b00.xyz', 'sto3g', charge=-1, spin=2)
+    mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'sto3g', charge=0, spin=0) ## test breaks when effective open-shell caluclation is needed
 
-    Xsad = atom.get_repr(mol, ["H", "O", "C", "I", "Rh"], -1, 2, dm=None,
-                      xc = 'hf', guess='sad', model='lowdin-long-x', auxbasis='def2tzvpjkfit')
+    Xsad = atom.get_repr(mol, ["H", "O"], 0, 0, dm=None,
+                      xc = 'hf', guess='sad', model='lowdin-long-x', auxbasis='ccpvdzjkfit')
 
     print(np.array([*Xsad[:,1]]).shape)
     return 0
 
+## This is the original failure (spotted) for a xyz-structutre from Reiher-group Monstanto DB
+#def test_other():
+#    print("Running weird-test")
+#    path = os.path.dirname(os.path.realpath(__file__))
+#    mol = compound.xyz_to_mol(path+'/data/645688c7c139c6414b5c0b00.xyz', 'sto3g', charge=-1, spin=2)
+#
+#    Xsad = atom.get_repr(mol, ["H", "O", "C", "I", "Rh"], -1, 2, dm=None,
+#                      xc = 'hf', guess='sad', model='lowdin-long-x', auxbasis='def2tzvpjkfit')
+#
+#    print(np.array([*Xsad[:,1]]).shape)
+#    return 0
+
 
 if __name__ == '__main__':
-    test_water()
-    test_other()
+    test_water_open_shell()
+    test_water_close_shell()
+    #test_other()
