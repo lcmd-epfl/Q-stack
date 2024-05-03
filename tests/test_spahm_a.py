ae7a0506b009a5e13e4e4cdb2bbe978540d3edad
Yannick Calvino Alonso <yannick.calvinoalonso@epfl.ch>
2024-05-03 15:10:52 +0200

    Add test for valence-only and single-element representations

--- tests/test_spahm_a.py
+++ tests/test_spahm_a.py
@@ -43,6 +43,30 @@ def test_water_SAD_guess_close_shell():
         assert(a[0] == a_true[0])                        # atom type
         assert(np.linalg.norm(a[1]-a_true[1]) < 1e-08)   # atom representations
 
+def test_water_single_element():
+    path = os.path.dirname(os.path.realpath(__file__))
+    mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'minao', charge=0, spin=None)
+
+    X = atom.get_repr(mol, ["H", "O"], 0, None, dm=None,
+                      guess='LB', model='lowdin-long-x', auxbasis='ccpvdzjkfit', only_z=['O']) #requesting reps for O-atom only
+
+    X_true = np.load(path+'/data/SPAHM_a_H2O/X_H2O.npy', allow_pickle=True)
+    a = X[0]
+    for a_true in X_true:
+        if a[0] == a_true[0]:                       # atom type
+            assert(np.linalg.norm(a[1]-a_true[1]) < 1e-08)   # atom representations
+
+def test_water_valence_only():
+    path = os.path.dirname(os.path.realpath(__file__))
+    mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'minao', charge=0, spin=None)
+
+    X = atom.get_repr(mol, ["H", "O"], 0, None, dm=None,
+                      guess='LB', model='lowdin-long-x', auxbasis='ccpvdzjkfit', valence_only=True)
+    X_true = np.load(path+'/data/SPAHM_a_H2O/X_H2O_valence-only.npy', allow_pickle=True)
+    assert(X.shape == X_true.shape)
+    for a, a_true in zip(X, X_true):
+        assert(a[0] == a_true[0])                        # atom type
+        assert(np.linalg.norm(a[1]-a_true[1]) < 1e-08)   # atom representations
 
 
 if __name__ == '__main__':
