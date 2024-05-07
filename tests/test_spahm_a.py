4216077204d19989f385aeda53fdeb02e54e75d7
Yannick Calvino Alonso <yannick.calvinoalonso@epfl.ch>
2024-05-07 10:08:25 +0200

    Revert (patched) "Add test for valence-only and single-element representations"
    This reverts commit ae7a0506b009a5e13e4e4cdb2bbe978540d3edad.
    Patch: keept fixed test for single-element

--- tests/test_spahm_a.py
+++ tests/test_spahm_a.py
@@ -57,17 +57,6 @@ def test_water_single_element():
         if a[0] == a_true[0]:                       # atom type
             assert(np.linalg.norm(a[1]-a_true[1]) < 1e-08)   # atom representations
 
-def test_water_valence_only():
-    path = os.path.dirname(os.path.realpath(__file__))
-    mol = compound.xyz_to_mol(path+'/data/H2O.xyz', 'minao', charge=0, spin=None)
-
-    X = atom.get_repr(mol, ["H", "O"], 0, None, dm=None,
-                      guess='LB', model='lowdin-long-x', auxbasis='ccpvdzjkfit', valence_only=True)
-    X_true = np.load(path+'/data/SPAHM_a_H2O/X_H2O_valence-only.npy', allow_pickle=True)
-    assert(X.shape == X_true.shape)
-    for a, a_true in zip(X, X_true):
-        assert(a[0] == a_true[0])                        # atom type
-        assert(np.linalg.norm(a[1]-a_true[1]) < 1e-08)   # atom representations
 
 
 if __name__ == '__main__':
@@ -75,4 +64,3 @@ if __name__ == '__main__':
     test_water_SAD_guess_close_shell()
     test_water_SAD_guess_open_shell()
     test_water_single_element()
-    test_water_valence_only()
