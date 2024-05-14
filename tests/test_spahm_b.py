033ac2ea30e5dbc947c167c47982c1340e5137bb
calvinoY <yannick.calvinoalonso@epfl.ch>
2024-05-14 16:27:28 +0200

    Add test for single-element and same basis (with test files)

--- tests/test_spahm_b.py
+++ tests/test_spahm_b.py
@@ -16,6 +16,35 @@ def test_water():
     for Xa, Xa_true in zip(X, X_true):
         assert(np.linalg.norm(Xa-Xa_true) < 1e-8) # evaluating representation diff as norm (threshold = 1e-8)
 
+def test_water_O_only():
+    path = os.path.dirname(os.path.realpath(__file__))
+    xyz_in = path+'/data/H2O.xyz'
+    mols = utils.load_mols([xyz_in], [0], [0], 'minao')
+    dms = utils.mols_guess(mols, [xyz_in], 'LB', spin=[0])
+    X = bond.bond(mols, dms, spin=[0], only_z=['O'])
+    assert(X.shape == (2, 1, 554))
+    X = np.hstack(X) # merging alpha-beta components for spin unrestricted representation #TODO: should be included into function not in main
+    X = np.squeeze(X) #contains a single elements but has shape (1,Nfeat)
+    true_file = path+'/data/H2O_spahm_b.npy_alpha_beta.npy'
+    X_true = np.load(true_file)
+    X_true = X_true[0]
+    assert(X_true.shape == X.shape)
+    for Xa, Xa_true in zip(X, X_true):
+        assert(np.linalg.norm(Xa-Xa_true) < 1e-8) # evaluating representation diff as norm (threshold = 1e-8)
+
+def test_water_same_basis():
+    path = os.path.dirname(os.path.realpath(__file__))
+    xyz_in = path+'/data/H2O.xyz'
+    mols = utils.load_mols([xyz_in], [0], [0], 'minao')
+    dms = utils.mols_guess(mols, [xyz_in], 'LB', spin=[0])
+    X = bond.bond(mols, dms, spin=[0], same_basis=True)
+    X = np.hstack(X) # merging alpha-beta components for spin unrestricted representation #TODO: should be included into function not in main
+    true_file = path+'/data/H2O_spahm_b_CCbas.npy_alpha_beta.npy'
+    X_true = np.load(true_file)
+    assert(X_true.shape == X.shape)
+    print(X_true.shape , X.shape)
+    for Xa, Xa_true in zip(X, X_true):
+        assert(np.linalg.norm(Xa-Xa_true) < 1e-8) # evaluating representation diff as norm (threshold = 1e-8)
 
 if __name__ == '__main__':
     test_water()
