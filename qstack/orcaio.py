01560209ef257787ddb2991e4aa5fd59e6b05aa5
Ksenia <ksenia.briling@epfl.ch>
2024-05-15 19:27:06 +0200

    Add Orca's .gbw file reader
    TODO: fix basis set order for def2svp (and maybe others?)

--- qstack/orcaio.py
+++ qstack/orcaio.py
@@ -1,6 +1,8 @@
+import struct
 import numpy as np
 from qstack.mathutils.matrix import from_tril
 from qstack.tools import reorder_ao
+
 
 def read_density(mol, basename, directory='./', version=500, openshell=False, reorder_dest='pyscf'):
     """Reads densities from an ORCA output.
@@ -43,3 +45,54 @@ def read_density(mol, basename, directory='./', version=500, openshell=False, re
 
     dm = np.squeeze(dm)
     return dm
+
+
+def _parse_gbw(fname):
+    """ Many thanks to
+    https://pysisyphus.readthedocs.io/en/latest/_modules/pysisyphus/calculators/ORCA.html
+    """
+
+    with open(fname, "rb") as f:
+        f.seek(24)
+        offset = struct.unpack("<q", f.read(np.int64().itemsize))[0]    # int64: pointer to orbitals
+        f.seek(offset)
+        sets   = struct.unpack("<i", f.read(np.int32().itemsize))[0]    # int32: number of MO sets
+        nao    = struct.unpack("<i", f.read(np.int32().itemsize))[0]    # int32: number of orbitals
+        assert sets in (1,2)
+
+        coefficients_ab = []
+        energies_ab = []
+        occupations_ab = []
+
+        for i in range(sets):
+            def read_array(n, dtype):
+                return np.frombuffer(f.read(dtype().itemsize * n), dtype=dtype)
+            coefficients = read_array(nao*nao, np.float64).reshape(-1, nao)
+            occupations  = read_array(nao,     np.float64)
+            energies     = read_array(nao,     np.float64)
+            irreps       = read_array(nao,     np.int32)
+            cores        = read_array(nao,     np.int32)
+            coefficients_ab.append(coefficients)
+            energies_ab.append(energies)
+            occupations_ab.append(occupations)
+
+        coefficients_ab = np.array(coefficients_ab)
+        energies_ab     = np.array(energies_ab)
+        occupations_ab  = np.array(occupations_ab)
+
+        return coefficients_ab, energies_ab, occupations_ab
+
+
+def reorder_coeff_inplace(c_full, mol, reorder_dest='pyscf'):
+    def _reorder_coeff(c):
+        # In ORCA, def2-TZVP for metal is not sorted wrt angular momenta. TODO add the fixes
+        for i in range(len(c)):
+            c[:,i] = reorder_ao(mol, c[:,i], src='orca', dest=reorder_dest)
+    [_reorder_coeff(c_full[i]) for i in range(c_full.shape[0])]
+
+
+def read_gbw(mol, fname, reorder_dest='pyscf'):
+    c, e, occ = _parse_gbw(fname)
+    if reorder_dest is not None:
+        reorder_coeff_inplace(c, mol, reorder_dest)
+    return c, e, occ
--- tests/test_orca.py
+++ tests/test_orca.py
@@ -5,7 +5,7 @@ import numpy as np
 import qstack
 
 
-def test_orca_reader():
+def test_orca_density_reader():
     path = os.path.dirname(os.path.realpath(__file__))
     mol = qstack.compound.xyz_to_mol(path+'/data/orca/H2O.xyz', 'sto3g', charge=1, spin=1)
     dm = qstack.fields.dm.get_converged_dm(mol, 'HF')
@@ -20,5 +20,24 @@ def test_orca_reader():
     assert(np.linalg.norm(dm504-dm421)<5e-3)
 
 
+def test_orca_gbw_reader():
+    path = os.path.dirname(os.path.realpath(__file__))
+    mol = qstack.compound.xyz_to_mol(path+'/data/orca/H2O.xyz', 'sto3g', charge=1, spin=1)
+
+    mf, _ = qstack.fields.dm.get_converged_mf(mol, 'HF')
+    c = mf.mo_coeff
+    e = mf.mo_energy
+    occ = mf.mo_occ
+
+    c504, e504, occ504 = qstack.orcaio.read_gbw(mol, path+'/data/orca/H2O.orca504.gbw')
+    for s in range(c.shape[0]):
+        for i in range(c.shape[-1]):
+            assert 5e-3 > min(np.linalg.norm(c[s,:,i]-c504[s,:,i]), np.linalg.norm(c[s,:,i] + c504[s,:,i]))
+
+    assert np.linalg.norm(e504-e) < 1e-5
+    assert np.linalg.norm(occ504-occ) == 0.0  # fine since they contain only 0.0 and 1.0
+
+
 if __name__ == '__main__':
-    test_orca_reader()
+    test_orca_density_reader()
+    test_orca_gbw_reader()
