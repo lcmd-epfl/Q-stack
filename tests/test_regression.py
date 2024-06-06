a87583a357e697ac56b98c097a92f9c64b218c4d
Ksenia <ksenia.briling@epfl.ch>
2024-06-06 12:17:31 +0200

    Add sparse regression test

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -7,6 +7,7 @@ import qstack.regression.regression as regression
 import qstack.spahm.compute_spahm as espahm
 import qstack.compound as compound
 
+
 def test_generate_reps():
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data/mols/')
     xyzlist = [os.path.join(path,s) for s in sorted(os.listdir(path)) if ".xyz" in s]
@@ -16,8 +17,8 @@ def test_generate_reps():
     X = np.array([np.pad(x, pad_width=(0,maxlen-len(x)), constant_values=0) for x in xmols])
     xfile = os.path.join(path, 'X_lb.npy')
     Xtrue = np.load(xfile)
-    #print(xyzlist)
     assert(np.allclose(X,Xtrue))
+
 
 def test_hyperparameters():
     path = os.path.dirname(os.path.realpath(__file__))
@@ -25,7 +26,7 @@ def test_hyperparameters():
     X = np.load(xfile)
     yfile = os.path.join(path, 'data/mols/dipole.dat')
     y = np.loadtxt(yfile)
-    
+
     hyper = hyperparameters.hyperparameters(X, y)[-4:]
     true_hyper = [  [5.18303885e-01,3.00507798e-01,1.00000000e-05,3.16227766e+01],
                     [5.18262897e-01,3.00473853e-01,3.16227766e-08,3.16227766e+01],
@@ -34,23 +35,38 @@ def test_hyperparameters():
 
     assert(np.allclose(hyper, true_hyper))
 
+
 def test_regression():
     path = os.path.dirname(os.path.realpath(__file__))
-    xfile = os.path.join(path, 'data/mols/X_lb.npy')
-    X = np.load(xfile)
-    yfile = os.path.join(path, 'data/mols/dipole.dat')
-    y = np.loadtxt(yfile)
-    
+    X = np.load(os.path.join(path, 'data/mols/X_lb.npy'))
+    y = np.loadtxt(os.path.join(path, 'data/mols/dipole.dat'))
+
     lc = regression.regression(X, y, sigma=3.162278e+01, eta=1.000000e+00, debug=True)
-    true_lc = [(1, 0.3858612698451199, 0.007817875402536187), (2, 0.3699378161120017, 0.10616950528187692),
-               (4, 0.3136096022855527, 0.07485650465620536), (6, 0.24018169400891018, 0.08584295185009833),
+    true_lc = [(1, 0.3858612698451199, 0.007817875402536187),
+               (2, 0.3699378161120017, 0.10616950528187692),
+               (4, 0.3136096022855527, 0.07485650465620536),
+               (6, 0.24018169400891018, 0.08584295185009833),
                (8, 0.2708852104417901, 7.021666937153402e-17)]
 
     assert(np.allclose(lc, true_lc))
 
-def main():
+
+def test_regression_sparse():
+    path = os.path.dirname(os.path.realpath(__file__))
+    X = np.load(os.path.join(path, 'data/mols/X_lb.npy'))
+    y = np.loadtxt(os.path.join(path, 'data/mols/dipole.dat'))
+
+    lc = regression.regression(X, y, sigma=3.162278e+01, eta=1.000000e+00, debug=True, sparse=5)
+    true_lc = [(1, 0.26949030187125617, 0.0037384513934352747),
+               (2, 0.4522608435324484, 0.3148074318684532),
+               (4, 0.4803773474666784, 0.19356070353924582),
+               (6, 0.333707374435793, 0.13803898307368923),
+               (8, 0.4501685644789055, 8.95090418262362e-17)]
+    assert(np.allclose(lc, true_lc))
+
+
+if __name__ == '__main__':
     test_generate_reps()
     test_hyperparameters()
     test_regression()
-
-if __name__ == '__main__': main()
+    test_regression_sparse()
