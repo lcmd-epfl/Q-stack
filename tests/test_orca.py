ad0cfbee7719ab9cad1c862e3905aa8a5a8bbe20
Ksenia <ksenia.briling@epfl.ch>
2025-07-28 15:16:48 +0200

    find fn in test orca

--- tests/test_orca.py
+++ tests/test_orca.py
@@ -23,8 +23,8 @@ def _dipole_moment(mol, dm):
 def test_orca_density_reader():
     path = os.path.dirname(os.path.realpath(__file__))
     mol = qstack.compound.xyz_to_mol(path+'/data/orca/H2O.xyz', 'sto3g', charge=1, spin=1)
-    dm = qstack.fields.dm.get_converged_dm(mol, 'HF')
-    dm = np.array([dm[0]+dm[1], dm[0]-dm[1]])
+    #dm = qstack.fields.dm.get_converged_dm(mol, 'HF')
+    #dm = np.array([dm[0]+dm[1], dm[0]-dm[1]])
 
     #dm400 = qstack.orcaio.read_density(mol, 'H2O.orca400', directory=path+'/data/orca/', version=400, openshell=True)
     #dm421 = qstack.orcaio.read_density(mol, 'H2O.orca421', directory=path+'/data/orca/', version=421, openshell=True)
