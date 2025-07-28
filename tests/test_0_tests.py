78078efb5a4f4addbff1af1cd5ba4361beab15d5
Ksenia <ksenia.briling@epfl.ch>
2025-07-28 15:28:29 +0200

    test test

--- tests/test_0_tests.py
+++ tests/test_0_tests.py
@@ -3,9 +3,21 @@
 import os
 import numpy as np
 import qstack
+from pyscf import dft
 
 
 def test_tests():
     path = os.path.dirname(os.path.realpath(__file__))
     mol = qstack.compound.xyz_to_mol(path+'/data/orca/H2O.xyz', 'sto3g', charge=1, spin=1)
-    dm = qstack.fields.dm.get_converged_dm(mol, 'HF')
+
+    xc = 'HF'
+    dm = qstack.fields.dm.get_converged_dm(mol, xc)
+
+    if mol.multiplicity == 1:
+        mf = dft.RKS(mol)
+    else:
+        mf = dft.UKS(mol)
+    mf.xc = xc
+    mf.verbose = 0
+    mf.kernel()
+    dm = mf.make_rdm1()
