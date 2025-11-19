ea2f6388815b9e7234e572c6ef8b6e84e96781c1
Liam Marsh <liam.marsh@epfl.ch>
2025-11-19 11:11:56 +0100

    fix molden test for other versions of pyscf
    Co-authored by: Liam Marsh <liam.marsh@epfl.ch>
    Co-authored by: Ksenia Briling <ksenia.briling@epfl.ch>

--- tests/test_molden.py
+++ tests/test_molden.py
@@ -2,7 +2,6 @@
 
 import os
 import tempfile
-import filecmp
 import numpy as np
 from qstack import compound
 from qstack.fields.density2file import coeffs_to_molden
@@ -14,7 +13,13 @@ def test_molden():
     c = np.load(path+'/data/H2O_dist.ccpvdz.ccpvdzjkfit.npy')
     tmpfile = tempfile.mktemp() + '.molden'
     coeffs_to_molden(auxmol, c, tmpfile)
-    assert (filecmp.cmp(path+'/data/H2O_dist.ccpvdz.ccpvdzjkfit.molden', tmpfile))
+    with open(tmpfile) as f:
+        output = f.readlines()
+        output = ''.join(output[0:1]+output[2:])
+    with open(path+'/data/H2O_dist.ccpvdz.ccpvdzjkfit.molden') as f:
+        output0 = f.readlines()
+        output0 = ''.join(output0[0:1]+output0[2:])
+    assert np.all(output==output0)
 
 
 if __name__ == '__main__':
