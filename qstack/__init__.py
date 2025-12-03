45875ac7edca67f26504d879c7cecbe200b984f8
Ksenia <briling@mail.ru>
2025-12-03 15:46:26 +0100

    Fix later versions compatibility

--- qstack/__init__.py
+++ qstack/__init__.py
@@ -1,12 +1,5 @@
 """Q-stack."""
 
 from qstack import tools
-#from qstack import compound  # needs pyscf
 from qstack import constants
 from qstack import mathutils
-#from qstack import fields  # needs scipy, pyscf
-#from qstack import basis_opt  # needs pyscf, scipy
-#from qstack import spahm  # requires pyscf
-#from qstack import orcaio  # requires pyscf
-#from qstack import qml  # requires ase
-#from qstack import regression  # requires sklearn (scikit-learn  as pip calls it)
