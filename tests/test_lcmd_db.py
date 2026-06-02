ac33bf870819dc20ad89b7d81cd86efadb58149a
calvinoY <yannick.calvinoalonso@epfl.ch>
2026-06-02 18:49:42 +0200

    add test for SMARTS-based query (in dataset)

--- tests/test_lcmd_db.py
+++ tests/test_lcmd_db.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from lcmd_db import load_dataset
+import os
 import polars as pl
+from lcmd_db import load_dataset
 
 
 def test_loading_dataset():
@@ -20,5 +21,22 @@ def test_loading_molecule():
     return None
 
 
+def test_molecule_query():
+    aromatic_smarts = "c1ccccc1"
+    tot_candidates = 7896
+    candidate_data = load_dataset("oscar_nhc", include=["molecules"], smarts=aromatic_smarts)
+    candidate_mols =  candidate_data.as_dataset("molecules")
+    assert (len(candidate_mols) == tot_candidates)
+    smiles = [mol.properties["smiles"] for mol in candidate_mols]
+    path = os.path.dirname(os.path.realpath(__file__))
+    with open(path+"/data/SMARTS_query-oscar_nhc-c1ccccc1.txt") as fout:
+        true_smiles = [l.strip("\n") for l in fout]
+    for smi in smiles:
+        assert (smi in true_smiles)
+    return None
+
+
 if __name__ == '__main__':
     test_loading_molecule()
+    test_loading_dataset()
+    test_molecule_query()
