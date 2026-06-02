6d4ed209d8552d9a8fff72ae691fe10c614cd56b
calvinoY <yannick.calvinoalonso@epfl.ch>
2026-06-02 17:33:56 +0200

    made test for `lcmd_db` dataset and mol imports

---
+++ tests/test_lcmd_db.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+from lcmd_db import load_dataset
+import lcmd_db
+import polars as pl
+
+
+def test_loading_dataset():
+    data = load_dataset("oscar_nhc")
+    molecules = data.as_dataset("molecules")
+    assert(len(molecules) == 8622)
+    return None
+
+def test_loading_molecule():
+    data = load_dataset("oscar_nhc")
+    molecules = data.as_dataset("molecules")
+    mol = molecules.filter(pl.col("id") == 178743)[0]
+    mol_smiles = "[C]1SN=C2COC[C@H](c3ccccc3)N12"
+    assert(mol_smiles==mol.properties["smiles"])
+    return None
+
+
+
+
+if __name__ == '__main__':
+    test_loading_molecule()
